33306bbf9e659e7634c5ef240cb0bf7b19b4d537
Suvankar Paul <shuvoenr@gmail.com>
2021-07-11 16:34:01 +0600

    Initiate

---
+++ main.go
@@ -0,0 +1,21 @@
+//main.go
+package main
+import (
+	"GO_API/Config"
+	"GO_API/Models"
+	"GO_API/Routes"
+	"fmt"
+	"github.com/jinzhu/gorm"
+)
+var err error
+func main() {
+	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+	if err != nil {
+		fmt.Println("Status:", err)
+	}
+	defer Config.DB.Close()
+	Config.DB.AutoMigrate(&Models.User{})
+	r := Routes.SetupRouter()
+	//running
+	r.Run()
+}
